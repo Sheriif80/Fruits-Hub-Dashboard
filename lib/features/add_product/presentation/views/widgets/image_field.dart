1875315201946bf3078acf1446c43240f18937d2
Sherif80 <sherifashraf1054@gmail.com>
2026-02-18 17:12:11 +0200

    implement image picking functionality and enhance image display in the add product view

--- lib/features/add_product/presentation/views/widgets/image_field.dart
+++ lib/features/add_product/presentation/views/widgets/image_field.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:fruits_hub_dashboard/core/helpers/pick_image_from_storage.dart';
+import 'package:fruits_hub_dashboard/core/utils/app_snackbars.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:skeletonizer/skeletonizer.dart';
 
@@ -11,6 +15,7 @@ class ImageField extends StatefulWidget {
 
 class _ImageFieldState extends State<ImageField> {
   bool _isLoading = false;
+  File? fileImage;
 
   @override
   Widget build(BuildContext context) {
@@ -20,22 +25,46 @@ class _ImageFieldState extends State<ImageField> {
         onTap: () async {
           setState(() => _isLoading = true);
           try {
-            final ImagePicker picker = ImagePicker();
-            final XFile? image = await picker.pickImage(
-              source: ImageSource.gallery,
-            );
-          } on Exception catch (e) {}
-          if (!mounted) return;
+            final XFile? image = await pickImageFromStorage();
+            if (image == null) {
+              setState(() => _isLoading = false);
+              return;
+            }
+            fileImage = File(image.path);
+            AppSnackbars.showSuccess(context, message: " Image was picked");
+          } catch (e) {
+            AppSnackbars.showError(context, message: "Image was not picked");
+          }
           setState(() => _isLoading = false);
         },
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(30),
-            border: Border.all(width: 0.4),
-          ),
-          height: 200,
-          width: double.infinity,
-          child: const Center(child: Icon(Icons.camera_alt, size: 50)),
+        child: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                border: Border.all(width: 0.4),
+              ),
+              height: 200,
+              width: double.infinity,
+              child: fileImage != null
+                  ? ClipRect(child: Image.file(fileImage!, fit: BoxFit.fill))
+                  : const Center(child: Icon(Icons.camera_alt, size: 50)),
+            ),
+            Visibility(
+              visible: fileImage != null,
+              child: Positioned(
+                top: 5,
+                left: 10,
+                child: IconButton(
+                  onPressed: () {
+                    setState(() => fileImage = null);
+                    AppSnackbars.showSuccess(context, message: "Image removed");
+                  },
+                  icon: const Icon(Icons.delete, color: Colors.red),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
