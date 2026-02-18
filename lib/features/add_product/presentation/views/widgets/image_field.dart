8e040642478259288d1db16a2a65fd9aeb2cd4a4
Sherif80 <sherifashraf1054@gmail.com>
2026-02-18 16:41:32 +0200

    added the skeltonizer package and now i can uplad photos from storage

--- lib/features/add_product/presentation/views/widgets/image_field.dart
+++ lib/features/add_product/presentation/views/widgets/image_field.dart
@@ -1,18 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
-class ImageField extends StatelessWidget {
+class ImageField extends StatefulWidget {
   const ImageField({super.key});
 
   @override
+  State<ImageField> createState() => _ImageFieldState();
+}
+
+class _ImageFieldState extends State<ImageField> {
+  bool _isLoading = false;
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(30),
-        border: Border.all(width: 0.4),
+    return Skeletonizer(
+      enabled: _isLoading,
+      child: GestureDetector(
+        onTap: () async {
+          setState(() => _isLoading = true);
+          try {
+            final ImagePicker picker = ImagePicker();
+            final XFile? image = await picker.pickImage(
+              source: ImageSource.gallery,
+            );
+          } on Exception catch (e) {}
+          if (!mounted) return;
+          setState(() => _isLoading = false);
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            border: Border.all(width: 0.4),
+          ),
+          height: 200,
+          width: double.infinity,
+          child: const Center(child: Icon(Icons.camera_alt, size: 50)),
+        ),
       ),
-      height: 200,
-      width: double.infinity,
-      child: const Center(child: Icon(Icons.camera_alt, size: 50)),
     );
   }
 }
