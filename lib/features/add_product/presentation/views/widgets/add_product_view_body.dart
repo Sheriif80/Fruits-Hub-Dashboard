e5dce481ec69f86b705d49358b1debf1e1c0c2ed
Sherif80 <sherifashraf1054@gmail.com>
2026-02-18 22:33:27 +0200

    Handeled the Validation of the form

--- lib/features/add_product/presentation/views/widgets/add_product_view_body.dart
+++ lib/features/add_product/presentation/views/widgets/add_product_view_body.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:fruits_hub_dashboard/core/utils/app_snackbars.dart';
+import 'package:fruits_hub_dashboard/core/widgets/custom_button.dart';
 import 'package:fruits_hub_dashboard/core/widgets/custom_text_form_field.dart';
 import 'package:fruits_hub_dashboard/features/add_product/presentation/views/widgets/image_field.dart';
 import 'package:fruits_hub_dashboard/features/add_product/presentation/views/widgets/is_featured_product.dart';
@@ -13,6 +17,11 @@ class AddProductViewBody extends StatefulWidget {
 
 class _AddProductViewBodyState extends State<AddProductViewBody> {
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  late String? productCode, productName, productDescription;
+  late num? productPrice;
+  File? fileImage;
+  bool isFeatured = false;
+
   AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
   @override
   Widget build(BuildContext context) {
@@ -24,30 +33,68 @@ class _AddProductViewBodyState extends State<AddProductViewBody> {
           autovalidateMode: autovalidateMode,
           child: Column(
             children: [
-              const CustomTextFormField(
-                hintText: 'Product ID',
+              CustomTextFormField(
+                onSaved: (value) {
+                  productCode = value!.toLowerCase();
+                },
+                hintText: 'Product Code',
                 keyboardType: TextInputType.number,
               ),
               const Gap(12),
-              const CustomTextFormField(
+              CustomTextFormField(
+                onSaved: (value) {
+                  productName = value;
+                },
                 hintText: 'Product Name',
                 keyboardType: TextInputType.text,
               ),
               const Gap(12),
-              const CustomTextFormField(
+              CustomTextFormField(
+                onSaved: (value) {
+                  productPrice = num.parse(value!);
+                },
                 hintText: 'Product Price',
                 keyboardType: TextInputType.number,
               ),
               const Gap(12),
-              const CustomTextFormField(
+              CustomTextFormField(
+                onSaved: (value) {
+                  productDescription = value;
+                },
                 maxLines: 5,
                 hintText: 'Product description',
                 keyboardType: TextInputType.text,
               ),
               const Gap(12),
-              ImageField(onImagePicked: (image) {}),
+              ImageField(
+                onImagePicked: (image) {
+                  fileImage = image;
+                },
+              ),
               const Gap(12),
               IsFeautredProduct(onChecked: (value) {}),
+              const Gap(12),
+              CustomButton(
+                text: "Add Product",
+                onPressed: () {
+                  if (fileImage != null) {
+                    if (formKey.currentState!.validate()) {
+                      formKey.currentState!.save();
+                    } else {
+                      autovalidateMode = AutovalidateMode.always;
+                      setState(() {});
+                    }
+                  } else {
+                    autovalidateMode = AutovalidateMode.always;
+                    setState(() {});
+                    AppSnackbars.showError(
+                      context,
+                      message: "Please Select an Image",
+                    );
+                  }
+                },
+              ),
+              const Gap(25),
             ],
           ),
         ),
