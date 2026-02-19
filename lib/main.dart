6589b79025e03f53a072d5f5a7b6c82d9651b6ad
Sherif80 <sherifashraf1054@gmail.com>
2026-02-19 13:30:41 +0200

    added supabase to the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:fruits_hub_dashboard/core/routing/router_generation_config.dart';
+import 'package:fruits_hub_dashboard/firebase_options.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Supabase.initialize(
+    url: 'https://smbbybqsrxyipefpasoc.supabase.co',
+    anonKey: 'sb_publishable_5Isk1AEvZfiX-numcrTmxg_sTJP1MJJ',
+  );
   runApp(const MyApp());
 }
 
