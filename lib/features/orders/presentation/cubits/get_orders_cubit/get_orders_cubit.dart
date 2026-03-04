3c4c9a14d287a939af33dbb4042cf419ca469860
Sherif80 <sherifashraf1054@gmail.com>
2026-03-04 02:30:05 +0200

    Used Stream Subscription the (get orders) cubit to controll and not leaving the stream always running

--- lib/features/orders/presentation/cubits/get_orders_cubit/get_orders_cubit.dart
+++ lib/features/orders/presentation/cubits/get_orders_cubit/get_orders_cubit.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:fruits_hub_dashboard/features/orders/domain/entities/order_entity.dart';
 import 'package:fruits_hub_dashboard/features/orders/domain/repos/orders_repo.dart';
@@ -9,9 +11,13 @@ class GetOrdersCubit extends Cubit<GetOrdersState> {
   GetOrdersCubit(this.ordersRepo) : super(GetOrdersInitial());
   final OrdersRepo ordersRepo;
 
-  void getOrders() async {
+  StreamSubscription? _streamSubscription;
+
+  void getOrders() {
     emit(GetOrdersLoading());
-    await for (final result in ordersRepo.getOrders()) {
+    // cancel the previous subscription if it exists
+    _streamSubscription?.cancel();
+    _streamSubscription = ordersRepo.getOrders().listen((result) {
       result.fold(
         (err) {
           emit(GetOrdersError(err.message));
@@ -20,6 +26,12 @@ class GetOrdersCubit extends Cubit<GetOrdersState> {
           emit(GetOrdersSuccess(orders));
         },
       );
-    }
+    });
+  }
+
+  @override
+  Future<void> close() {
+    _streamSubscription?.cancel();
+    return super.close();
   }
 }
